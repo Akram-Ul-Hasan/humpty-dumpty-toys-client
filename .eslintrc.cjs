360821f38cb5280e6bf8954c7b8c459a74ee5d8f
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-05-18 01:56:56 +0600

    added navbar and footer

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,7 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 }
