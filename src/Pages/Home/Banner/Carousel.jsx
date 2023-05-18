b8fae5f88e00bb8d1db6fe06fa898be341f31449
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-05-18 13:03:03 +0600

    added carousel in header section in home page

---
+++ src/Pages/Home/Banner/Carousel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Carousel = ({ img, id }) => {
+  const Id = parseInt(id);
+  return (
+    <div id={`slide${Id}`} className="carousel-item relative w-full">
+      <img src={img} className="w-full" />
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href={`#slide${Id === 1 ? 6 : Id - 1}`} className="btn btn-circle bg-slate-200 hover:bg-orange-400 text-black">
+          ❮
+        </a>
+        <a href={`#slide${(Id % 6) + 1}`} className="btn btn-circle">
+          ❯
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
